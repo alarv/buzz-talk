ada9981add9775cf7e5502fa906fff71c8143a9a
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2023-12-07 14:22:13 +0200

    fix: minor ui things

--- src/app/components/MessageCard.tsx
+++ src/app/components/MessageCard.tsx
@@ -8,12 +8,12 @@ interface MessageCardProps {
 
 export default function MessageCard(props: MessageCardProps) {
   return (
-    <div className="p-3 w-56 h-56 " style={{ background: '#DDF7FF' }}>
+    <div className="p-3 w-56 h-56 text-sm" style={{ background: '#DDF7FF' }}>
       {props.message && (
         <>
           <p>Channel: {props.message.publicChannelName}</p>
-          <p>From: {props.message.sourceUsername}</p>
-          <p>Content: {props.message.content}</p>
+          <p className="my-2">From: {props.message.sourceUsername}</p>
+          <p className="my-2">Content: {props.message.content}</p>
         </>
       )}
     </div>
